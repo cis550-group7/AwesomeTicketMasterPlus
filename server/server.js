da00ac4e277bf4f92cb98b83e59e4040860a5b50
serenliu <66586810+serenliu@users.noreply.github.com>
2022-12-05 19:33:38 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -38,16 +38,41 @@ app.get('/venues/', routes.getVenuesByName)
 // Route 9 - register as GET 
 app.get('/follows', routes.getFollows)
 
-// Route 2 - register as GET 
-app.post('/follow/', routes.createNewUser)
+// Route 10 - register as GET 
+app.post('/follow/', routes.followArtist)
 
+// Route 11 - register as GET 
+app.get('/reservations', routes.getReservations)
 
+// Route 12 - register as GET 
+app.post('/reservation/', routes.reserveEvent)
 
+// Route 13 - register as GET 
+app.delete('/reservation/', routes.unreserveEvent)
 
+// Route 14 - register as GET 
+app.get('/song', routes.getSongs)
 
+// Route 15 - register as GET 
+app.get('/topArtists/', routes.getPopularArtists)
 
+// Route 16 - register as GET 
+app.get('/upcomingEvents/', routes.getUpcomingEvents)
 
+// Route 17 - register as GET 
+app.get('/search/events', routes.search_events)
 
+// Route 18 - register as GET 
+app.get('/artists/rank', routes.rankArtistByEventCounts)
+
+// Route 19 - register as GET 
+app.get('/artist', routes.getArtistByName)
+
+// Route 20 - register as GET 
+app.get('/artist/songs', routes.getArtistSongs)
+
+// Route 20 - register as GET 
+app.get('/artist/events', routes.getArtistEvents)
 
 app.listen(config.server_port, () => {
     console.log(`Server running at http://${config.server_host}:${config.server_port}/`);
