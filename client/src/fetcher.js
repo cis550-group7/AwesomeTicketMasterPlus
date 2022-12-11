f20af0dad2eddf73ad0fa2e9e621852e15c8875c
Yuling <104866320+toritori777@users.noreply.github.com>
2022-12-11 09:04:25 -0800

    Add files via upload
    updated client/fetcher/js; not fully done

--- client/src/fetcher.js
+++ client/src/fetcher.js
@@ -1,64 +1,153 @@
 import config from './config.json'
 
-const getAllMatches = async (page, pagesize, league) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/matches/${league}?page=${page}&pagesize=${pagesize}`, {
+const getUser = async (id) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/user?id=${id}`, {
         method: 'GET',
     })
     return res.json()
 }
 
-const getAllPlayers = async (page, pagesize) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/players?page=${page}&pagesize=${pagesize}`, {
+//createNewUser
+
+
+const getFollows = async (id) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/follows?id=${id}`, {
         method: 'GET',
     })
     return res.json()
 }
 
-const getMatch = async (id) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/match?id=${id}`, {
+//followArtist
+
+//unfollowArtist
+
+const getReservations = async (id) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/reservations?id=${id}`, {
         method: 'GET',
     })
     return res.json()
 }
 
-const getPlayer = async (id) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/player?id=${id}`, {
+//reserveEvent
+
+//unreserveEvent
+
+const getArtist = async (id) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/artist?id=${id}`, {
         method: 'GET',
     })
     return res.json()
 }
 
-const getMatchSearch = async (home, away, page, pagesize) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/search/matches?Home=${home}&Away=${away}&page=${page}&pagesize=${pagesize}`, {
+const getArtistsByName = async (name) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/artists?name=${name}`, {
         method: 'GET',
     })
     return res.json()
 }
 
-const getPlayerSearch = async (name, nationality, club, rating_high, rating_low, pot_high, pot_low, page, pagesize) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/search/players?Name=${name}&Nationality=${nationality}&Club=${club}&RatingLow=${rating_low}&RatingHigh=${rating_high}&PotentialHigh=${pot_high}&PotentialLow=${pot_low}&page=${page}&pagesize=${pagesize}`, {
+const getSongs = async (name) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/songs?name=${name}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+
+const getEvent = async (id) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/event?id=${id}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+
+const getEventsByName = async (name) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/events?name=${name}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+
+const getVenue = async (id) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/venue?id=${id}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+
+const getVenuesByName = async (name) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/venues?name=${name}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+
+const getPopularArtists = async (N) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/artists/topPopular?N=${N}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+
+const getArtistsByNumEvents = async (N) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/artists/mostEvents?N=${N}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+
+const getUpcomingEvents = async () => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/events/upcoming`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+
+const search_events = async (eventName, country) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/events/search?Name=${eventName}&Country=${country}`, {
         method: 'GET',
     })
     return res.json()
 }
 
 
+const rankArtistByEventCounts = async () => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/rank`, {
+        method: 'GET',
+    })
+    return res.json()
+}
 
+const getArtistSongs = async (artist) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/songs/${artist}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
 
-
-
-
-
-
-
-
+const getArtistEvents = async (artist) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/events/${artist}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
 
 
 export {
-    getAllMatches,
-    getAllPlayers,
-    getMatch,
-    getPlayer,
-    getMatchSearch,
-    getPlayerSearch
+    getUser,
+    getFollows,
+    getReservations,
+    getArtist,
+    getArtistsByName,
+    getSongs,
+    getEvent,
+    getEventsByName,
+    getVenue,
+    getVenuesByName,
+    getPopularArtists,
+    getArtistsByNumEvents,
+    getUpcomingEvents,
+    search_events,
+    rankArtistByEventCounts,
+    getArtistSongs,
+    getArtistEvents
 }
